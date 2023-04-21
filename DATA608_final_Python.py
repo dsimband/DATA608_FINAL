bc455849d77ec0807d980c98ebdc7c17c60142a8
dsimband <dsimbandumwe@gmail.com>
2023-04-20 22:24:07 -0400

    Update DATA608_final_Python.py

--- DATA608_final_Python.py
+++ DATA608_final_Python.py
@@ -16,7 +16,6 @@ import dash_bootstrap_components as dbc
 
 
 
-dir_output = './output/trip_map.html'
 
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -25,11 +24,17 @@ server = app.server
 
 
 
+def create_table(df):
+    columns, values = df.columns, df.values
+    header = [html.Tr([html.Th(col) for col in columns])]
+    rows = [html.Tr([html.Td(cell) for cell in row]) for row in values]
+    table = [html.Thead(header), html.Tbody(rows)]
+    return table
+
+
 # Config 
 station_lst = ['7617.07']
 bike_file = 'https://raw.githubusercontent.com/dsimband/DATA608_FINAL/main/data/bike.csv'
-
-
 
 
 
@@ -49,10 +54,18 @@ app.layout = html.Div(id = 'parent', children = [
 
 
     html.Div([dcc.Graph(id = 'forcast_plot')] ,style = {'textAlign':'center','marginTop':10,'marginBottom':10}),   
-    html.Div(["Input: ",dcc.Input(id='input-m', value='initial value m', type='text')]),
+    html.Div(["Input: ",dcc.Input(id='input_m', value='initial value m', type='text')]),
               
-    
-    
+    html.Div([
+        html.Div([
+            dmc.Table(
+                id = 'bike_table',
+                striped=True,
+                highlightOnHover=True,
+                withBorder=False,
+                withColumnBorders=True,
+        ),], style={'width': '95%', 'float': 'center', 'display': 'inline-block','textAlign':'center'}),
+    ]),  
         
     ],style = {'textAlign':'center','marginTop':20,'marginBottom':20,'marginLeft':20,'marginRight':20,
               'font-size': 10,})
@@ -60,6 +73,7 @@ app.layout = html.Div(id = 'parent', children = [
     
 
 @app.callback(
+    Output("bike_table", "children"),
     Output("forcast_plot", "figure"),
     Input("input_m", "value"),
 )
@@ -68,9 +82,12 @@ def graph_update(m):
     #print(bike_df.shape)
     fig = px.bar(bike_df, y='bikes_chng', height=800)
     fig.update_layout(template="simple_white", title="Bike Demand")
+    
+    
+    bike_table = create_table(bike_df)
 
     
-    return fig
+    return bike_table, fig
     
    
 
